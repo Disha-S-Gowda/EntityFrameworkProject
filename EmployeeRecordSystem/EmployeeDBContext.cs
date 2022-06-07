1f2156f99f517808a9c34fdc0213badb991611e8
Disha_S <Disha_S@dell.com>
2022-06-07 11:08:42 +0530

    initial commit

---
+++ EmployeeRecordSystem/EmployeeDBContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeRecordSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeRecordSystem
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeRecordSystem/Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeRecordSystem.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Salary { get; set; }
+    }
+}
